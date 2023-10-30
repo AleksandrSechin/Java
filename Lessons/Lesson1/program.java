fb3049b0456be39c54f0d6c69a5c34b377826464
Alex <ansechin2012@ya.ru>
2023-10-30 15:08:06 +0300

    java1

--- Lessons/Lesson1/program.java
+++ Lessons/Lesson1/program.java
@@ -258,5 +258,123 @@ public class Program {
             System.out.println(text);
             iScanner.close();
         }
-    }
+}
 
+// Цикл while
+public class Program {
+    public static void main(String[] args) {
+        int value = 321;
+        int count = 0;
+        while (value != 0) {
+            value /= 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
+
+// Цикл do while
+public class Program {
+    public static void main(String[] args) {
+        int value = 321;
+        int count = 0;
+        do {
+            value /= 10;
+            count++;
+        } while (value != 0);
+        System.out.println(count);
+    }
+}
+
+// Выполнение следующей итерации цикла — continue.
+// Прерывание текущей итерации цикла — break.
+// * ближайшего к оператору
+
+// Оператор цикла for
+public class Program {
+    public static void main(String[] args) {
+        int s = 0;
+        for (int i = 1; i <= 10; i++) {
+            s += i;
+        }
+        System.out.println(s);
+    }
+}
+
+// Вложенные циклы
+public class Program {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // * * * * *
+        // * * * * *
+        // * * * * *
+        // * * * * *
+        // * * * * *
+    }
+}
+
+// for : Работает только для коллекций
+public class Program {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        for (int item : arr) {
+            System.out.printf("%d ", item);
+        }
+        System.out.println();
+    }
+}
+
+// Работа с файлами Создание и запись\ дозапись
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+ public static void main(String[] args) {
+ try (FileWriter fw = new FileWriter("file.txt", false)) {
+ fw.write("line 1");
+ fw.append('\n');
+ fw.append('2');
+ fw.append('\n');
+ fw.write("line 3");
+ fw.flush();
+ } catch (IOException ex) {
+ System.out.println(ex.getMessage());
+ }
+ } }
+
+// Чтение, Вариант посимвольно
+import java.io.*;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("file.txt");
+        int c;
+        while ((c = fr.read()) != -1) {
+            char ch = (char) c;
+            if (ch == '\n') {
+                System.out.print(ch);
+            } else {
+                System.out.print(ch);
+            }
+        }
+    }
+}
+
+// Вариант построчно
+import java.io.*;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.printf("== %s ==\n", str);
+        }
+        br.close();
+    }
+}
